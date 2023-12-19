cfe5179855f5635009278bcd3754bdc9244f7474
Atanas (Tony) Hristov <atanas.y.hristov@gmail.com>
2023-12-18 21:29:47 -0600

    Vue 3 Mastery - 2.26 - List rendering v-for

---
+++ vue-3-mastery/02-basics/02.26-list-rendering-v-for/Notes.md
@@ -0,0 +1,7 @@
+# Notes
+
+## 02.26-list-rendering-v-for
+
+Syntax is: `v-for="(item, index) of items`.
+
+Note: when order of element changes, the list re-renders as well.
---
+++ vue-3-mastery/02-basics/02.26-list-rendering-v-for/app.js
@@ -0,0 +1,58 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const vm = Vue.createApp({
+  data() {
+    return {
+      items:
+      [
+        {
+          name: 'Milk',
+          price: 1.5
+        },
+        {
+          name: 'Beer',
+          price: 3.1
+        },
+        {
+          name: 'Juice',
+          price: 2.5
+        }
+      ],
+    }
+  },
+  methods: {
+    addSomeItems(cnt) {
+      for (let i = 0; i < cnt; i++) {
+        this.items.push({
+          name: `Item ${i}`,
+          price: getRandomInt(100) + getRandomInt(99) / 100
+        });
+      }
+    },
+    sortByPrice() {
+      function cmp(a, b) {
+        if (a.price < b.price) {
+          return -1;
+        } else if (a.price > b.price) {
+          return 1;
+        }
+        return 0;
+      }
+      this.items.sort(cmp);
+    },
+    sortByName() {
+      function cmp(a, b) {
+        if (a.name < b.name) {
+          return -1;
+        } else if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      }
+      this.items.sort(cmp);
+    }
+
+  }
+}).mount('#app');
