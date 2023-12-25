3fdf1a28b7252d6d2f59c4bda59cb3dd7a57db63
Atanas (Tony) Hristov <atanas.y.hristov@gmail.com>
2023-12-24 18:17:37 -0600

    Vue 3 Mastery - 5.58 - Creating links

---
+++ vue-3-mastery/05-vue-router/05.58-creating-links/Notes.md
@@ -0,0 +1,11 @@
+# Notes
+
+## 05.58-creating-links
+
+Use elemenrt
+
+Example:
+
+```html
+<div><RouterLink to="/">Home Page</RouterLink></div>
+```
---
+++ vue-3-mastery/05-vue-router/05.58-creating-links/router/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div><RouterLink to="/">Home Page</RouterLink></div>
+    <div><RouterLink to="/about">About Page</RouterLink></div>
+  </div>
+  <RouterView />
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
